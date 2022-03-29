d8000555b8051d113dc9d7a9aff4bee4cd472d87
alihassan-here <alihassanuog50@gmail.com>
2022-03-29 19:30:24 +0500

    setup redux and get dummy data

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,12 +1,15 @@
 import ProjectList from "../projects/ProjectList"
-import Notifications from "./Notifications"
+import Notifications from "./Notifications";
+import { useSelector } from "react-redux";
+
 
 const Dashboard = () => {
+    const { projects } = useSelector((state) => state.project);
     return (
         <div className='dashboard container'>
             <div className='row'>
                 <div className='col s12 m6'>
-                    <ProjectList />
+                    <ProjectList projects={projects} />
                 </div>
                 <div className='col s12 m5 offset-m1'>
                     <Notifications />
@@ -17,4 +20,4 @@ const Dashboard = () => {
     )
 }
 
-export default Dashboard
+export default Dashboard;
